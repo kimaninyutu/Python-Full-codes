23b4d3910053273bf590d014c99cb9cf91753e88
kimani nyutu <93473945+kimaninyutu@users.noreply.github.com>
2022-08-24 12:09:32 +0300

    Delete Find_number_factors.py

--- Find_number_factors.py
+++
@@ -1,7 +0,0 @@
-def print_factors(x):
-    print(f"The factors of {x} are : ")
-    for i in range(1, x+1, 1):
-        if x % i == 0:
-            print(i)
-num = int(input("Enter a number to find the factors : "))
-print_factors(num)
